87d3f647db2792c8ee888e71324323b80d05a641
Chris <christopher.cortsen@gmail.com>
2022-09-22 18:42:34 +0200

    Finished Linq

--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,28 +2,50 @@ namespace Assignment2;
 
 public class Queries
 {
-    public static IEnumerable<String> Rowling1(IEnumerable<Wizard> wzCol)=> 
+    public static IEnumerable<string> Rowling1(IEnumerable<Wizard> wzCol)=> 
     from wz in wzCol
     where wz.Creator.Contains("Rowling")
     select  wz.Name 
     ;
 
-    public static IEnumerable<String> Rowling2(IEnumerable<Wizard> wzCol)=> throw new NotImplementedException();
-    //wzCol.Where(x=>x.Creator.Contains("Rowling")).ToList();
+    public static IEnumerable<string> Rowling2(IEnumerable<Wizard> wzCol)=>
+    wzCol.Where(x=>x.Creator.Contains("Rowling")).Select(x=>x.Name);
 
-    public static int Sith1(IEnumerable<Wizard> wzCol)=>throw new NotImplementedException()
+    public static int Sith1(IEnumerable<Wizard> wzCol)=>
+    wzCol.Where(x=>x.Name.Contains("Darth")).Select(x => x.Year).Min().GetValueOrDefault()
+    ;
+
+    public static int Sith2(IEnumerable<Wizard> wzCol)=>
+    (   from wz in wzCol
+        where wz.Name.Contains("Darth")
+        orderby wz.Year ascending
+        select wz.Year).Min().GetValueOrDefault()
+    ;
+
+    public static IEnumerable<(string,int?)> HarryPotter1(IEnumerable<Wizard> wzCol)=>
+        wzCol.Where(x => x.Medium.Contains("Harry Potter")).Select(x => new List<(string,int?)>{(x.Name , x.Year)}).SelectMany(x=>x.Distinct())
+    ;
+
+    public static IEnumerable<(string,int?)> HarryPotter2(IEnumerable<Wizard> wzCol)=> 
+        from wz in wzCol
+        where wz.Medium.Contains("Harry Potter")
+        select (wz.Name, wz.Year)
+    ;
+
+    public static IEnumerable<string> reverseOrder1(IEnumerable<Wizard> wzCol)=> 
+        (from wz in wzCol
+        group wz by new {wz.Name,wz.Creator} into g
+        orderby g.Key.Creator descending, g.Key.Name ascending
+        select g.Key.Name)
 
     ;
 
-    public static int Sith2(IEnumerable<Wizard> wzCol)=> throw new NotImplementedException();
-
-    public static IEnumerable<T> HarryPotter1<T>()=> throw new NotImplementedException();
-
-    public static IEnumerable<T> HarryPotter2<T>()=> throw new NotImplementedException();
-
-    public static IEnumerable<T> reverseOrderByNameThenWiz1<T>()=> throw new NotImplementedException();
-
-    public static IEnumerable<T> reverseOrderByNameThenWiz2<T>()=> throw new NotImplementedException();
+    public static IEnumerable<string> reverseOrder2(IEnumerable<Wizard> wzCol)=>
+             wzCol.OrderByDescending(x=>x.Creator)
+             .GroupBy(grp => grp.Creator)
+            .SelectMany(g => g.OrderBy(grp => grp.Name))
+            .Select(x=>x.Name)
+    ;
 
 
 }
